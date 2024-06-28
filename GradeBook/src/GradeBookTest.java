65293c2398ae6d97bf9a435b8ed709ea6a2c9165
BrayanS <145617007+Brayan-Sanchez@users.noreply.github.com>
2024-06-28 12:56:31 -0500

    Add files via upload

---
+++ GradeBook/src/GradeBookTest.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class GradeBookTest {
+    public static void main(String[] args) {
+        GradeBook courseName1 = new GradeBook("numero2");
+        System.out.println(courseName1.displayMessage() + courseName1.getCourseName());
+        Scanner input = new Scanner(System.in);
+
+        double total;
+        int gradeCounter;
+        double average;
+        double grade;
+        int pass;
+        int notPass;
+
+        total = 0;
+        gradeCounter = 1;
+
+        pass = 0;
+        notPass= 0;
+
+        while(gradeCounter <=10){
+            System.out.println("Escriba la nota estudiante " + gradeCounter);
+            grade = input.nextDouble();
+            if(grade >=3){
+                pass = pass + 1;
+            }else{
+                notPass = notPass + 1;
+            }
+
+            total = total + grade;
+            gradeCounter = gradeCounter + 1;
+
+        }
+        average = total / 10 ;
+
+        System.out.println("El promedio de las notas es: " + average);
+        if(pass >= 8){
+            System.out.println("El profesor merece el bono");
+        }else if(notPass <8){
+            System.out.println("El profesor No merece el bono");
+        }
+    }
+}
